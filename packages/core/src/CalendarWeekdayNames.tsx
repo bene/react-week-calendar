218ac6359b0770cbd04381a0b79c379571280335
Benedict Allerberger <me@bene.dev>
2023-02-14 14:27:05 +0100

    Add `_twInclude`

--- packages/core/src/CalendarWeekdayNames.tsx
+++ packages/core/src/CalendarWeekdayNames.tsx
@@ -63,4 +63,14 @@ function CalendarWeekdayNames({
   );
 }
 
+const _twInclude = [
+  "grid-cols-1",
+  "grid-cols-2",
+  "grid-cols-3",
+  "grid-cols-4",
+  "grid-cols-5",
+  "grid-cols-6",
+  "grid-cols-7",
+];
+
 export default CalendarWeekdayNames;
